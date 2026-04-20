aa58917cce449cd006b205777d4c7312ce671c05
lunakittywolf <melody.m.moseley@gmail.com>
2026-04-19 19:39:13 -0500

    Pointless Rythm Game Added

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,3 +1,47 @@
+
+let heyTrigger = function(){
+    console.log("Hey!")
+    $("#testSound").on("click",function(){
+        $("#settings").addClass("dancing")
+        setTimeout(function(){
+            $("#settings").removeClass("dancing");
+        },500)
+    })
+    setTimeout(function(){
+        $("#testSound").off("click");
+        $("#testSound").on("click",function(){
+            playSound("Hey")
+        })
+    },500)
+}
+
+
+let startHeyCheck = function(){
+    setTimeout(function(){
+        let times = 0;
+        heyCheck = setInterval(function(){
+            times++;
+            console.log(times)
+            if(times>7){
+                clearInterval(heyCheck);
+                times = 0;
+                heyCheck = setInterval(function(){
+                    heyTrigger();
+                    times++
+                    if(times>13){
+                        clearInterval(heyCheck);
+                        heyCheck = setTimeout(function(){
+                            heyTrigger();
+                        },14000)
+                    }
+                },1000)
+            }
+            heyTrigger();
+        },8000)
+
+    },7400)
+}
+
 customPlaySong = async function(){
     song = document.getElementById("song");
     song.volume = .25;
@@ -5,6 +49,7 @@ customPlaySong = async function(){
         song.volume = localStorage.getItem("musicVolume")
     }
     song.play().then(() => {
+        startHeyCheck();
         setTimeout(function(){
             changeName();
             changeName();
@@ -17,6 +62,7 @@ customPlaySong = async function(){
         $("body").append("<div id='music-popup'>You have autoplay turned off! Click anywhere for music! :3</div>")
         setTimeout(animateMusicPopup,100);
         $('body').on("click", function(){
+            startHeyCheck();
             setTimeout(function(){
                 changeName();
                 changeName();
@@ -33,8 +79,10 @@ customPlaySong = async function(){
 customPlaySong();
 song.addEventListener("ended", function(){
     clearInterval(nameChange);
+    clearInterval(heyCheck);
     song.currentTime = 0;
     song.play().then(() => {
+        startHeyCheck();
         setTimeout(function(){
             setInterval(changeName, 1000)
         },500)
